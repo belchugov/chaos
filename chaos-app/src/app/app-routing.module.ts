02c325354d0f64fd7e97c689c618dde0cd9a41c9
Dimitar Belchugov <d.belchugov@gmail.com>
2020-01-24 14:50:27 +0200

    Add admin component, update styles, Add routing

--- chaos-app/src/app/app-routing.module.ts
+++ chaos-app/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomepageComponent} from "./homepage/homepage.component";
+import {AdminComponent} from "./admin/admin.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'homepage',
+    component: HomepageComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  },
+  {
+    path: '',
+    redirectTo: '/homepage',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
