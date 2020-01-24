021267b22e6a3ebb5a6d77c9506500b72943a968
Lyubomir Dimitrov <lyubomir.dimitrov@garaio.com>
2020-01-24 13:38:24 +0200

    add get by id and post endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,38 @@
-const IPFS = require("ipfs");
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const ipfsAPI = require("ipfs-api");
+const fs = require("fs");
 const faker = require("faker");
 
-const express = require("express");
 const app = express();
-const port = 3000;
+app.use(fileUpload());
 
-app.get("/", function(req, res) {
-  res.send(getFakePosts(3));
+const port = 3000;
+const ipfs = ipfsAPI({
+  host: "localhost",
+  port: 5001,
+  protocol: "http"
 });
 
-app.get("/post", function(req, res) {
-  res.send(getFakePosts(1)[0]);
+app.get("/post/:id", function(req, res) {
+  ipfs.files.get(req.params.id, (err, files) => {
+    if (err) {
+      res.send("Unable to find file");
+    }
+    files.forEach(file => {
+      res.send(file.content.toString("utf8"));
+    });
+  });
 });
 
 app.post("/post", function(req, res) {
-  res.send("Got a POST request");
-});
-
-app.delete("/post", function(req, res) {
-  res.send("Got a DELETE request at /user");
+  let buffer = Buffer.from(req.files.article.data)
+  ipfs.files.add(buffer, function(err, file) {
+    if (err) {
+      console.log(err);
+    }
+    res.send(file);
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
@@ -28,7 +42,7 @@ function getFakePosts(number) {
   for (let i = 0; i < number; i++) {
     array.push({
       title: faker.lorem.word(),
-      body: faker.lorem.paragraphs(),
+      body: faker.lorem.paragraph(),
       date: faker.date.recent(10)
     });
   }
